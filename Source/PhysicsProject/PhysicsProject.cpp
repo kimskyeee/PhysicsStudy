799c3e8bd5f0d8592a723f5b106da9763548cc20
kimskyeee <zlagksnf@gmail.com>
2025-08-25 19:09:13 +0900

    Initial commit: clean history

---
+++ Source/PhysicsProject/PhysicsProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PhysicsProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PhysicsProject, "PhysicsProject" );
